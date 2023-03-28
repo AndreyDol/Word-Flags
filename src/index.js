083ab5dac341c56f9537786c8054558bcccd37ce
AndreyDol <113788568+AndreyDol@users.noreply.github.com>
2023-03-28 09:44:24 +0300

    add chesk field

--- src/index.js
+++ src/index.js
@@ -1,97 +1,116 @@
 // Создай фронтенд часть приложения поиска данных о стране по её частичному или полному имени.
 
-import { fetchCountries } from './js/fetchCountries';
+//import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
-import debounce from 'lodash.debounce';
+//import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const random = data => Math.floor(Math.random() * (data.length - 1));
+
+let foundData = {};
+//Запрос даних
+const fetchUsers = async () => {
+  const response = await fetch(`https://restcountries.com/v3.1/all`);
+  const users = await response.json();
+  return users;
+};
+
+const doStuff = async () => {
+  try {
+    foundData = await fetchUsers();
+    console.log(users);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+doStuff();
 
 // Пошук поля вводу та місця для додавання списку в DOM
 const refs = {
   name: document.querySelector('#search-box'),
   countriesList: document.querySelector('.country-list'),
   countriesInfo: document.querySelector('.country-info'),
+  button: document.querySelector('.button'),
 };
 
-// Додає слухача та використовує функцію debounce, яка робить HTTP-запит через 300мс після того, як користувач перестав вводити текст
-refs.name.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+function generateCheckCountry(data, oneCounry, mount) {
+  let arrayCheckCountry = [];
+  let randomCountryNumber;
+  const randomI = Math.floor(Math.random() * mount);
+  for (let i = 0; i < mount; i++) {
+    if (i === randomI) randomCountryNumber = oneCounry;
+    else randomCountryNumber = random(foundData);
+    arrayCheckCountry.push(data[randomCountryNumber].name.official);
+  }
+
+  return arrayCheckCountry;
+}
+
+// Додає слухача на кнопку
+refs.button.addEventListener('click', onSearch);
 
 function onSearch(event) {
-    // записуємо в змінну введене значення користувачем (trim прибирає пробіли)
-    let inputEl = refs.name.value.trim();
+  // очистка списку країн та інформації про країну
+  //refs.countriesInfo.innerHTML = '';
+  refs.countriesList.innerHTML = '';
 
-// очистка списку країн та інформації про країну
-    refs.countriesInfo.innerHTML = "";
-    refs.countriesList.innerHTML = "";
+  // перевірка, якщо значення не пустий рядок
 
-// перевірка, якщо значення не пустий рядок
-    
-if(inputEl !== ""){
-    fetchCountries(inputEl).then(foundData => {
-      // якщо масив більше 10 країн, то відображається повідомлення
-      if (foundData.length > 10) {
-        // використання бібліотеки Notiflix для відображення повідомлення
-        Notiflix.Notify.info(
-          `Too many matches found. Please enter a more specific name`
-        );
-      }
-      // якщо масив більше 2 та меньше 10 країн, то виводить розмітку (прапор, країна)
-      if ((foundData.length > 2) & (foundData.length < 10)) {
-        insertContent(foundData);
-      }
-      // якщо масив з 1 країни, то виводить інформацією для однієї країни (прапор, назва, столиця, населення, мови)
-      if (foundData.length === 1) {
-        insertContentInfo(foundData);
-      }
-        // якщо користувач ввів не иснуючу назву країни, то відображає повідомлення
-      if (foundData.length === 0) {
-        Notiflix.Notify.failure(`Oops, there is no country with that name`);
-      }
-    })
+  if (foundData !== {}) {
+    console.log(foundData);
+    const randomCountry = random(foundData);
+
+    console.log(generateCheckCountry(foundData, randomCountry, 5));
+    console.log(randomCountry);
+    console.log(foundData[randomCountry]);
+    insertContent(
+      foundData[randomCountry],
+      generateCheckCountry(foundData, randomCountry, 5)
+    );
+
+    if (foundData.length === 0) {
+      Notiflix.Notify.failure(`Oops, there is no country with that name`);
+    }
   }
- };
-    // Розмітка для однієї країни
-    const createListCountries =
-      item => `<li class="list list__info"><img src="${item.flags.png}" alt="${item.flags.alt}" width="30" height="20"/>
-  <p class = "text">  ${item.name.official}</p></li>`;
-        
-   
-
-// Перебір масива
-const generateContent = (array) => array.reduce((acc, item) => {
-    return acc + createListCountries(item);
-}, "");
-
-
-// Додавання в DOM
-const insertContent = (array) => {
-    const result = generateContent(array);
-    refs.countriesList.insertAdjacentHTML('beforeend', result);
-};
-
-
-// Розмітка з інформацією для однієї країни (прапор, назва, столиця, населення, мови)
-const createInfoCountries = item =>
-    `<li class="list"><img src="${
-  item.flags.png
-}" alt="${item.flags.alt}" width="60" hight="40"/>
-  <p class="name"><b>${item.name.official}</b></p>
-  <p><b>Capital</b>: ${item.capital}</p>
-   <p><b>Population</b>: ${item.population}</p>
-   <p><b>Languages</b>: ${Object.values(item.languages).join(", ")}</p></li>
+}
+// Розмітка для однієї країни
+const createListCountries = (
+  item,
+  checkContry
+) => `<li class="list list__info"><img src="${item.flags.png}" alt="${item.flags.alt}" width="300" height="200"/>
+  </li>
+   <h2>   </h2>
+   <form>
+  <label>
+      <input type="radio" name="question">
+      ${checkContry[0]}
+    </label>
+  <br>
+  <label>
+      <input type="radio" name="question">
+       ${checkContry[1]}
+    </label>
+     <br>
+     <label>
+      <input type="radio" name="question">
+       ${checkContry[2]}
+    </label>
+     <br>
+     <label>
+      <input type="radio" name="question">
+       ${checkContry[3]}
+    </label>
+     <br>
+     <label>
+      <input type="radio" name="question">
+      ${checkContry[4]}
+    </label>
+</form>
   `;
 
-
-  // Перебір масива
-const generateContentInfo = (array) => array.reduce((acc, item) => {
-   
-    return acc + createInfoCountries(item);
-}, "");
-
-
 // Додавання в DOM
-const insertContentInfo = (array) => {
-    const result = generateContentInfo(array);
-    refs.countriesInfo.insertAdjacentHTML('beforeend', result);
+const insertContent = (array, checkContry) => {
+  const result = createListCountries(array, checkContry);
+  refs.countriesList.insertAdjacentHTML('beforeend', result);
 };
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,20 +1,18 @@
-
-export const fetchCountries = (name) =>{
-    return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    ).then(response => {
-        if (!response.ok) {
-            if (response.status === 404) {
-                return [];
-            }
+export const fetchCountries = name => {
+  return fetch(
+    `https://restcountries.com/v3.1/all`
+    // `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
+        }
         throw new Error(response.status);
       }
       return response.json();
-    }) 
- .catch(error => {
+    })
+    .catch(error => {
       console.error(error);
     });
 };
-
-
-
